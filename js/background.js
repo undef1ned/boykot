0b0e3c90a0e8ab93c1addbd989c87eb1b87f9604
unknown <the.cooljames@gmail.com>
2014-02-17 23:05:42 +0200

    BL update

--- js/background.js
+++ js/background.js
@@ -1,5 +1,68 @@
-var boykot = {
-    _shit: [
+(function(){
+    var _shit = [
+
+
+        "*://*.eho-ua.com/*",
+        "*://*.enterfilm.com.ua/*",
+        "*://*.europaplus.ua/*",
+        "*://*.fbc.net.ua/*","*://*.felicita.net.ua/*",
+        "*://*.footballua.tv/*",
+        "*://*.formula-smaku.com/*","*://*.gadyach-cheese.com/*","*://*.gazovik.kiev.ua/*","*://*.glas.org.ua/*",
+        "*://*.grand.sevlush.net/*","*://*.hol-yar.com.ua/*",
+        "*://*.intergal-bud.com.ua/*",
+        "*://*.jamfm.com.ua/*",
+        "*://*.kirova.biz/*","*://*.korolevskoe.com/*","*://*.kp.ua/*","*://*.kulik-moloko.net.ua/*","*://*.kyivhlib.com.ua/*","*://*.laska.ua/*",
+        "*://*.lebed.dn.ua/*","*://*.legrand-dnepr.com.ua/*","*://*.leleka.lg.ua/*","*://*.livs.com.ua/*","*://*.lkt.lg.ua/*","*://*.maitai.com.ua/*",
+        "*://*.malahyt.com.ua/*","*://*.mariyayaremchuk.com/*","*://*.mega-apteka.com/*","*://*.megatv.ua/*","*://*.mezpology.zp.ua/*","*://*.mgu.com.ua/*",
+        "*://*.molbufet.com.ua/*","*://*.mykolayivhlib.pat.ua/*","*://*.nashe.ua/*","*://*.national.kharkov.ua/*","*://*.newsone.ua/*","*://*.newsradio.com.ua/*",
+        "*://*.nl.irtafax.com.ua/*","*://*.nlotv.com/*","*://*.nord.ua/*","*://*.nsvet.com.ua/*","*://*.odessa36.tv/*","*://*.oiff.com.ua/*",
+        "*://*.parangon.org/*","*://*.partyzone.ua/*","*://*.phl.ua/*","*://*.pjs.com.ua/*","*://*.portofino.lg.ua/*","*://*.primorie.odessa.ua/*",
+        "*://*.probka.ua/*","*://*.proun.com.ua/*","*://*.ra-pozitiv.com.ua/*","*://*.radiomelodia.com.ua/*","*://*.radioroks.com.ua/*",
+        "*://*.retro.ua/*","*://*.rusradio.com.ua/*","*://*.santimo.ck.ua/*","*://*.sfgkolos.com/*",
+        "*://*.strahovigarantii.com.ua/*","*://*.super-radio.com.ua/*","*://*.tamtam.com.ua/*","*://*.toksudak.crimea.com/*",
+        "*://*.trcfabrika.com.ua/*","*://*.tv7.kharkov.ua/*","*://*.tyachivnews.in.ua/*","*://*.ukr-t2.net/*","*://*.ukraflora.com.ua/*","*://*.ukrzoloto.ua/*",
+        "*://*.vareniki.mk.ua/*","*://*.vecherka.donetsk.ua/*","*://*.vecherniy.kharkov.ua/*",
+        "*://*.vidikon.sumy.ua/*","*://*.vuso.ua/*","*://*.vz.ua/*","*://*.wog.ua/*",
+        "*://*.3bears.com.ua/*","*://*.3mob.net.ua/*","*://*.abucb.com/*",
+        "*://*.andromed.odessa.ua/*","*://*.ark-spa.com/*","*://*.arma-motors.kiev.ua/*",
+        "*://*.avantazh.ua/*","*://*.batiktea.com.ua/*","*://*.bebkoauto.com/*","*://*.bfontanov.com/*","*://*.bilozgar.com.ua/*",
+        "*://*.biofarma.ua/*","*://*.biosphere-corp.com/*","*://*.brsm-nafta.com/*","*://*.bs-hotel.com.ua/*",
+        "*://*.cheboturka.com.ua/*","*://*.cheeseclub.ua/*","*://*.coopinvest.com.ua/*","*://*.dbf.com.ua/*","*://*.delod.odessa.net/*",
+        "*://*.gadzhiev-bus.com/*","*://*.galychyna.com.ua/*","*://*.garantiya.com.ua/*","*://*.goldengate.kharkov.ua/*",
+        "*://*.grishko.in.ua/*","*://*.hitfm.ua/*","*://*.hotel-marat.com.ua/*","*://*.ichnya.com/*","*://*.ilitashtour.com.ua/*",
+        "*://*.illichivska.com.ua/*","*://*.k1.ua/*","*://*.khladoprom.com.ua/*","*://*.kissfm.ua/*","*://*.komo.ua/*",
+        "*://*.kreditmarket.ua/*","*://*.kum.com.ua/*","*://*.kyivopt.com/*","*://*.landgutgroup.com/*","*://*.lanita.com.ua/*",
+        "*://*.lasoschi.com.ua/*","*://*.lider-zhk.com.ua/*","*://*.loungefm.com.ua/*","*://*.lugcity.com.ua/*","*://*.maristella-club.com/*",
+        "*://*.marshalok.com.ua/*","*://*.mashstroy.com.ua/*","*://*.mayak.pl.ua/*","*://*.media-inform.tv/*",
+        "*://*.newprivoz.com/*","*://*.novalinia.com.ua/*","*://*.ogvr.com.ua/*","*://*.olimp.ua/*",
+        "*://*.opanas.com.ua/*",
+        "*://*.oranta-lugan.com.ua/*","*://*.pankurchak.ua/*","*://*.parallel.ua/*","*://*.parkovemisto.com/*","*://*.parkovyozera.com.ua/*",
+        "*://*.parus.biz/*","*://*.pervak.kiev.ua/*","*://*.pr.ua/*",
+        "*://*.prime-vodka.com/*","*://*.radiorelax.com.ua/*","*://*.rassvet.by/*",
+        "*://*.rivstone.com.ua/*","*://*.rosinka.ua/*","*://*.sarepta.ua/*","*://*.sputnik.lg.ua/*","*://*.sudak-aquapark.com/*",
+        "*://*.sundolina.com/*","*://*.swizer.ua/*","*://*.tairovo.com.ua/*",
+        "*://*.tele.ru/*","*://*.telesensventures.com/*","*://*.tonis.ua/*",
+        "*://*.tv.irtafax.com.ua/*","*://*.ubb.com.ua/*","*://*.udp.ua/ru/object/4/*","*://*.ukrbus.com/*",
+        "*://*.vbr-bank.com.ua/*","*://*.veresfood.com/*","*://*.viknoplus.ua/*","*://*.vogue.ua/*","*://*.vostok.lg.ua/*",
+        "*://*.yaskraviy.com/*","*://*.zebra.ua/*","*://*.yan-tabachnik.com.ua/*","*://*.zolotaforel.ua",
+
+
+        "*://*.112.ua/*",
+        "*://*.34.ua/*",
+        "*://*.apk.karlovka.info/*",
+        "*://*.askoldtea.com.ua/*",
+        "*://*.ukravto.ua/*",
+        "*://*.babai-family.com.ua/*",
+        "*://*.barabashovo.ua/*",
+        "*://*.bighockey.ua/*",
+        "*://*.brusnichka.com.ua/*",
+        "*://*.btb.tv/*",
+        "*://*.ctdominanta.com.ua/*",
+        "*://*.cxid.info/*",
+        "*://*.dnews.donetsk.ua/*",
+        "*://*.donbass.tv/*",
+        "*://*.edem.kharkov.ua/*",
+        "*://*.egoist.com.ua/*",
         "*://*.i.ua/*",
         "*://*.bigmir.net/*",
         "*://*.oll.tv/*",
@@ -47,17 +110,49 @@ var boykot = {
 		"*://*.voloshkovepole.com.ua/*",
 		"*://*.veresfood.com/*", "*://*veres-group.com/*",
 		"*://*.tas-insurance.com.ua/*",
-		"*://*.obozrevatel.com/*"
-    ],
-    init: function(){
+		"*://*.obozrevatel.com/*",
+        "*://*.aif.ua/*",
+        "*://*.amstor.ua/*",
+        "*://*.amstor.zakaz.ua/*",
+        "*://*.apk-invest.com.ua/*",
+        "*://*.apteka-tas.com.ua/*",
+        "*://*.aska.com.ua/*",
+        "*://*.bankpershiy.com.ua/*",
+        "*://*.bckiev.com.ua/*",
+        "*://*.brusnichka.com.ua/*",
+        "*://*.capital.ua/*",
+        "*://*.chernomorets.odessa.ua/*",
+        "*://*.express-insurance.com.ua/*",
+        "*://*.fcgoverla.uz.ua/*",
+        "*://*.fcilich.com/*",
+        "*://*.fcmetalurg.com/*",
+        "*://*.fcsevastopol.com/*",
+        "*://*.metalist.ua/*",
+        "*://*.mistobank.com.ua/*",
+        "*://*.mogilevskaya.com.ua/*",
+        "*://*.mtochka.com.ua/*",
+        "*://*.nasha-apteka.com.ua/*",
+        "*://*.poplavskiy.com/*",
+        "*://*.povaliy.com.ua/*",
+        "*://*.pravex.com/*",
+        "*://*.rccf.ua/*",
+        "*://*.realbank.com.ua/*",
+        "*://*.shakhtar.com/*",
+        "*://*.stirolbiopharm.com.ua/*",
+        "*://*.tascombank.com.ua/*",
+        "*://*.unexbank.ua/*",
+        "*://*.zarya-lugansk.com/*"
+    ];
+
+    this.init = function(){
 
         chrome.browserAction.onClicked.addListener(function(){
             chrome.tabs.create({
-                url:    "https://www.facebook.com/BoycotteInUkraine"
+                url:    "https://facebook.com/BoycotteInUkraine"
             })
         });
 
-        this._shit.forEach(function(pattern){
+        _shit.forEach(function(pattern){
             chrome.webRequest.onBeforeRequest.addListener(function(details) {
                     console.log(details);
                     switch (details.type){
@@ -88,5 +183,7 @@ var boykot = {
                 ["blocking"]
             );
         });
-    }
-}.init();
+    };
+
+    this.init();
+})();
